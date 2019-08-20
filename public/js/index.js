b4551537bd63fc408f6a97809fc9075ee2ea6342
booldook <booldook@gmail.com>
2019-08-20 14:11:05 +0900

    daily 가져오기

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,11 @@ function cityInit() {
 
 // 전역변수
 var ajax = new XMLHttpRequest();
-
+var dailyAjax = new XMLHttpRequest();
+var weeklyAjax = new XMLHttpRequest();
+var key = '02efdd64bdc14b279bc91d9247db4722';
+var dailyAPI = 'https://api.openweathermap.org/data/2.5/weather';
+var weeklyAPI = 'https://api.openweathermap.org/data/2.5/forecast';
 // main - 도시정보 가져오기
 cityInit();
 function cityInit() {
@@ -46,6 +50,49 @@ function cityInit() {
 			*/
 
 			// ES6: select#cities 에 도시를 option으로 추가하기
+			// var citySelect = document.querySelector("#cities");
+			var elem;
+			var cityName;
+			elem = document.createElement('option');
+			cityName = document.createTextNode("날씨를 검색할 도시 이름을 선택해 주세요.");
+			elem.appendChild(cityName);
+			elem.setAttribute("value", "");
+			elem.setAttribute("selected", "selected");
+			citySelect.appendChild(elem);
+			for(var i in cities) {
+				// tag를 만든다. 단 DOM에 적용되지 전단계
+				elem = document.createElement('option');
+				// tag 안에 삽입될 텍스트를 만든다.	
+				cityName = document.createTextNode(cities[i].name);
+				// 생성된 tag에 속성을 준다.
+				elem.setAttribute("value", cities[i].id);
+				// 생성된 tag에 생성된 텍스트를 붙인다.
+				elem.appendChild(cityName);
+				// 생성된 tag를 원하는 DOM의 Element에 붙인다.
+				citySelect.appendChild(elem);
+			}
+
+			// jQuery select change 이벤트
+			/*
+			$("#cities").change(function(){
+				console.log(	$(this).val()	);
+			});
+			*/
+
+			// ES5, ES6 change 이벤트
+			// citySelect.addEventListener(이벤트, 콜백함수)
+			citySelect.addEventListener("change", function(){
+				var cityId = this.value;
+				var dailyURL = dailyAPI + "?appid=" + key + "&id=" + cityId;
+				dailyAjax.onreadystatechange =  function(){
+					if (this.readyState == 4 && this.status == 200) {
+						var daily = JSON.parse(this.responseText);
+						console.log(daily);
+					}
+				};
+				dailyAjax.open("GET", dailyURL, true);
+				dailyAjax.send();
+			});
 		}
 	};
 	ajax.open("GET", "json/city.json", true);
